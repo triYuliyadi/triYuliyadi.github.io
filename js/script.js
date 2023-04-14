ab0da2cc4fd0140a61b1b4d3679ef8e752b829fe
Wisnu Giri <wisnugiri64@gmail.com>
2023-04-15 00:31:39 +0700

    tambah skrip dan ubah style css

--- js/script.js
+++ js/script.js
@@ -63,3 +63,28 @@ let swiper = new Swiper(".mySwiper", {
     },
 });
 
+                    //! ========== dark light mode ==========
+
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () =>{
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+}
+
+
+                    //! ========== Scroll Reveal ==========
+                    
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 1500,
+    delay: 150
+});
+
+ScrollReveal().reveal('.home-content , .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1 , .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p , .about-content', { origin: 'right' });
+
+
